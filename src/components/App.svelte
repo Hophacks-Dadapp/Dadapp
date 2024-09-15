cc946a9cf006c30ab61828faa1226787e6bf439e
tomicgun <thomasmartenbean@gmail.com>
2024-09-14 22:40:14 -0400

    Dad jokes throbber and some UI html stuff

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,26 +1,32 @@
 <script lang=js>
     import { onMount } from 'svelte'
     import { Loader } from '@googlemaps/js-api-loader'
-    
+    import {getDadJokes} from "../js/getdadjokes.js";
+
     const loader = new Loader({
-        apiKey: "API_KEY_HERE",
+        apiKey: "API-KEY",
         version: "weekly"
     });
-      
+
+    let dadJokeBox
     onMount(async () => {
         await loader.load()
-
         const { Map } = await google.maps.importLibrary("maps");
-        
+        setInterval(async () => {dadJokeBox.innerText = await getDadJokes()},5000)
         const map = new Map(document.getElementById("map"), {
             center: { lat: -34.397, lng: 150.644 },
             zoom: 23,
         });
     })
-</script>
 
+
+</script>
+<h2> Map </h2>
 
 <div id="map"></div>
+
+<h4> Dad joke </h4>
+<p bind:this = {dadJokeBox}></p>
 
 <style>
     #map {
